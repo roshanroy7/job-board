3ed56916e1868d61d2436177d6d6816e8ccb337e
roshanroy7 <roshanpalem@gmail.com>
2026-04-26 16:59:37 +0100

    using dynamic PORT for Railway delpoyment

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ app.get("/api/jobs/:id", async (req, res) => {
     }
 })
 
-app.listen(3001, () => {
-    console.log("Server running on http://localhost:3001")
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
